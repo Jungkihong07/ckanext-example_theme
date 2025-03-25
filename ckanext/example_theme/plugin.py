7db0ae4919c4f02f06891dd580b6ff1a2e654515
kihong <hnn07185@gmail.com>
2025-03-25 09:02:36 +0000

    Home page overload work + css work

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -1,22 +1,40 @@
+# encoding: utf-8
+
+'''plugin.py
+
+'''
+from ckan.common import CKANConfig
 import ckan.plugins as plugins
+# CKAN의 플러그인 툴킷 모듈을 가져옵니다 .
 import ckan.plugins.toolkit as toolkit
 
 
 class ExampleThemePlugin(plugins.SingletonPlugin):
+    '''An example theme plugin.
+
+    '''
+    # Declare that this class implements IConfigurer.
     plugins.implements(plugins.IConfigurer)
-    
 
-    # IConfigurer
-    class ExampleThemePlugin(plugins.SingletonPlugin):
-        pass
-    # '''An example theme plugin.
+    def update_config(self, config: CKANConfig):
 
-    # '''
-    # pass
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        # 'templates' is the path to the templates dir, relative to this
+        # plugin.py file.
+        toolkit.add_template_directory(config, 'templates')
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, "templates")
-        toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "example_theme")
+        # Add this plugin's public dir to CKAN's extra_public_paths, so
+        # that CKAN will use this plugin's custom static files.
+        toolkit.add_public_directory(config, 'public')
+
+
+        # Register this plugin's assets directory with CKAN.
+        # Here, 'assets' is the path to the webassets directory
+        # (relative to this plugin.py file), and 'example_theme' is the name
+        # that we'll use to refer to this assets directory from CKAN
+        # templates.
+        toolkit.add_resource('assets', 'example_theme')
+
 
     
